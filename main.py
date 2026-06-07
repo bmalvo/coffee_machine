3f6e5f1076b616febd385e0a36b87ba475b00837
bmalvo <boydmalvo@gmail.com>
2026-06-07 19:09:45 +0200

    change name coffee_kind to coffee_type

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # code will be here
+
+# 1 user input
+
+def coffee_machine():
+    coffee_type = input('What would you like? ([e]spresso/[l]atte/[c]appuccino): ')
+    return print(coffee_type)
+
+not_broken = True
+
+while not_broken:
+    coffee_machine()
