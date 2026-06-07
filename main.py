bd241d777e8525d449a3efb4d1daf8b27589026b
bmalvo <boydmalvo@gmail.com>
2026-06-07 19:46:00 +0200

    add max ingredients capacity and recipe three flavors

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 # code will be here
+# ingredients capacity: 3000ml - water, 2000ml - milk, 1000g - coffee
+#  recipes:
+#   espresso :
+#         50ml - water
+#         18g - cofee
+#         $1.50 - cost
+#   late :
+#         200ml - water
+#         24g - cofee
+#         150ml - milk
+#         $2.50 - cost
+#   cappuccino :
+#         250ml - water
+#         24g - cofee
+#         100ml - milk
+#         $3.00 - cost
 
 # 1 user input
+# 2 build 'off' button
+
 
 def coffee_machine():
-    coffee_type = input('What would you like? ([e]spresso/[l]atte/[c]appuccino): ')
-    return print(coffee_type)
+    not_broken = True
+    while not_broken:
+        coffee_type = input('What would you like? ([e]spresso/[l]atte/[c]appuccino): ')
+        print(coffee_type)
+        if coffee_type == 'off':
+            not_broken = False
+    return
 
-not_broken = True
-
-while not_broken:
-    coffee_machine()
+coffee_machine()
