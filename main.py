b82c04c85f2bea21ab1f656d02340309415b167f
bmalvo <boydmalvo@gmail.com>
2026-06-10 22:28:02 +0200

    Process coins.

--- main.py
+++ main.py
@@ -20,11 +20,12 @@
 # 2 build 'off' button
 # 3 print report
 # 4 Check resources sufficient
+# 5 Process coins
 
 
 def coffee_machine():
     not_broken = True
-    water = 30 # 3000 max
+    water = 300 # 3000 max
     milk = 200 # 2000 max
     coffee = 100 # 1000 max
     money = 0
@@ -45,19 +46,26 @@ def coffee_machine():
             'milk': 100,
             'price': 3.00}
     }
+    coins = {
+        'q': 0.25,
+        'd': 0.10,
+        'n': 0.05,
+        'p': 0.01,
+        'f': 0
+    }
     while not_broken:
         coffee_type = input('What would you like? ([e]spresso/[l]atte/[c]appuccino): ')
         print(recipes[coffee_type])
         if coffee_type in recipes:
             if water - recipes[coffee_type]['water'] < 0:
                 print('Sorry there is not enough water.')
-                not_broken = False
+                break
             if coffee - recipes[coffee_type]['coffee'] < 0:
                 print('Sorry there is not enough coffee.')
-                not_broken = False
+                break
             if milk - recipes[coffee_type]['milk'] < 0:
                 print('Sorry there is not enough milk.')
-                not_broken = False
+                break
         if coffee_type == 'off':
             not_broken = False
         if coffee_type == 'report':
@@ -67,6 +75,24 @@ def coffee_machine():
             Coffee: {coffee}g
             Money: ${money}
             """)
+        pay = 0
+        coin = True
+        print(f'''
+        Please insert coins. ${recipes[coffee_type]['price']}
+        Press "q" for quarter, "d" for dime, "n" for nickel,
+        "p" for penni and "f" if You have finished                
+        ''')
+        while coin != 0:
+            coin = input('insert coin: ')
+            if coin in coins.keys():
+                pay += coins[coin]
+                print(f'coin {coins[coin]}')
+                print(f'pay {pay}')
+            else:
+                print('Incorrect coin')
+            if coin == 'f':
+                break
+        print(f'inserted coins: {pay}')
     return
 
 coffee_machine()
